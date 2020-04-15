19435f3d08b8ece79db9a5500a17c2ab1fcb584c
maxjamb <maxwelljamesbeard@gmail.com>
2020-04-15 22:52:42 +0100

    stock prices and recipe batches done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,38 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # does ingredients have enough amounts for recipe in each key
+    # Create a variable to hold amount each ingredient value is divisible by recipe value
+    # batches_dictionary = {}
+    # Create a variable to hold the maximum number of batches
+    # maximum_batches = 0
+    # Loop through recipe matching each key to it's matching ingredients key
+    # for (k, v), (k2, v2), in zip(recipe.items(), ingredients.items()):
+    if recipe.keys() != ingredients.keys():
+        maximum_batches = 0
+    else:
+        batches_dictionary = (
+            {k: int(ingredients[k]/recipe[k]) for k in recipe})
+    # Set the results to the batches_dictionary variable
+    # batches_dictionary = {k: int(v2/v)}
+
+        maximum_batches = min(list(batches_dictionary.values()))
+    return maximum_batches
+
+    # recipe_dictionary - will show the ingredients and amounts needed
+    # ingredients_dictionary - will show the ingredients you have and amounts available
+
+    # output = maximum number of whole batches that can be made for the supplied recipe
+
+    # input = dictionary of ingredients
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 200, 'butter': 480, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,35 @@
 
 import argparse
 
+prices = [2, 8, 3, 5, 10]
+
+
 def find_max_profit(prices):
-  pass
+
+    # input = list of stock prices
+
+    # keep track of the current min price so far as we loop through
+
+    current_min_price = prices[0]
+    current_max_profit = prices[1] - prices[0]
+    for i in range(0, len(prices)):
+        if prices[i] < current_min_price:
+            current_min_price = prices[i]
+        if prices[i] - current_min_price > current_max_profit and prices[i] != current_min_price:
+            current_max_profit = prices[i] - current_min_price
+
+    return current_max_profit
 
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+# output = find max profit
+
+# if __name__ == '__main__':
+#   # This is just some code to accept inputs from the command line
+#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
+#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+#   args = parser.parse_args()
+
+#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
